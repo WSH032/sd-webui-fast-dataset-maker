f6c059ef67504c98d99e288ff68d049e594f5356
WSH032 <614337162@qq.com>
2023-05-07 02:44:40 +0800

    auto_select
    添加启发式自动选择重复图片功能

--- process_images.py
+++ process_images.py
@@ -6,59 +6,100 @@ Created on Fri May  5 17:38:31 2023
 """
 
 
+# image_dir = r"C:\Users\WSH\Desktop\B站文件\冷门参数"
+# image_list = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.lower().endswith(('.jpg', '.png', '.jpeg', '.bmp'))]
+
+IMAGE_EXTENSION = ('.jpg', '.png', '.jpeg', '.bmp')
+
 import gradio as gr
-import os
+
 
 choose_image_index = ""
-images_tuple_list = []
+cluster_list = []
 
 
-def find_duplicates_images(images_dir: str):
+def find_duplicates_images(images_dir: str, use_cache:bool):
 
-    global images_tuple_list
-
-    from imagededup.methods import PHash
     import os
-    import toml
-    # 载入模型
-    phasher = PHash()
-    # 检测重复
-    duplicates = phasher.find_duplicates(images_dir)
-    # 只保留确实有重复的图片
-    indeed_duplicates_set = [set(v).union({k})
-                             for k, v in duplicates.items() if v]
-    # 将重复的图片聚类
-    cluster = []
-    for s in indeed_duplicates_set:
-        for m in cluster:
-            if s & m:
-                m.union(s)
-                break
-        else:
-            cluster.append(s)
+    
+    def cluster_duplicates(images_dir: str) -> list:
+        """ 返回该目录下重复图像聚类，为一个集合列表，每个集合内为重复图像名字（不包含路径） """
+        
+        from imagededup.methods import PHash
+        
+        # 载入模型
+        phasher = PHash()
+        # 编码
+        global encodings
+        encodings = phasher.encode_images(image_dir=images_dir)
+        # 查找重复
+        duplicates = phasher.find_duplicates(encoding_map=encodings)
+        # 只保留确实有重复的图片,并弄成集合列表
+        indeed_duplicates_set = [set(v).union({k}) for k, v in duplicates.items() if v]
+        # 将重复的图片聚类
+        cluster_list = []
+        for s in indeed_duplicates_set:
+            for m in cluster_list:
+                if s & m:
+                    m.union(s)
+                    break
+            else:
+                cluster_list.append(s)
+        return cluster_list
 
+    global cluster_list
+    
+    cluster_list = cluster_duplicates(images_dir)
+    
+
+    def cache_image( input_dir: str, cluster_list: list, resolution: int=512 ):
+        """ 如果使用缓存，就调用pillow，将重复的图片缓存到同路径下的一个cache文件夹中，分辨率最大为512,与前面图片名字一一对应 """
+        
+        from PIL import Image
+        
+        # 建一个文件夹
+        cache_dir = os.path.join(input_dir, "cache")
+        os.makedirs(cache_dir, exist_ok=True)
+        
+        for cluster in cluster_list:
+            for image_name in cluster:
+                with Image.open( os.path.join( input_dir, image_name ) ) as im:
+                    im.thumbnail( (resolution, resolution) )
+                    im.save( os.path.join(cache_dir, image_name) )
+                    
+    
+    def cluster_to_gallery(images_dir: str, cluster_list: list) -> list:
     # 将图片的绝对路径和索引合成一个元组，最后把全部元组放入一个列表，向gradio.gallery传递
-    images_tuple_list.clear()
-    parent_index = 0
-    for parent in cluster:
-        son_index = 0
-        for son in parent:
-            images_tuple_list.append(
-                (os.path.join(images_dir, son), f"{parent_index}:{son_index}"))
-            son_index += 1
-        parent_index += 1
-
+        images_tuple_list = []
+        parent_index = 0
+        for parent in cluster_list:
+            son_index = 0
+            for son in parent:
+                images_tuple_list.append( (os.path.join(images_dir, son), f"{parent_index}:{son_index}") )
+                son_index += 1
+            parent_index += 1
+        return images_tuple_list
+    
+    # 如果使用缓存就缓存图像
+    if use_cache:
+        cache_image(images_dir, cluster_list, resolution=512)
+    
+    # 如果用缓存就展示缓存图
+    gallery_images_dir = images_dir if not use_cache else os.path.join( images_dir, "cache" )
+    images_tuple_list = cluster_to_gallery( gallery_images_dir, cluster_list)
+    
     # 根据聚类数,生成如下字典
     """
     "1":[]
     "2":[]
     ...
     """
-    delet_images_dict = {f"{i}": [] for i in range(parent_index)}
-    delet_images_str = toml.dumps(delet_images_dict)
-
-    return images_tuple_list, ""
+    # delet_images_dict = {f"{i}": [] for i in range( len(cluster_list) )}
+    # delet_images_str = toml.dumps(delet_images_dict)
     # return images_tuple_list, delet_images_str
+    
+    return images_tuple_list, ""
+    
 
 
 def confirm(delet_images_str: str) -> str:
@@ -92,15 +133,54 @@ def cancel(delet_images_str: str) -> str:
     delet_images_dict = toml.loads(delet_images_str)
     # 把删除标志如 "0:1" 分成 "0" 和 "1"
     [parent_index_str, son_index_str] = choose_image_index.split(":")[0:2]
-
+    # 如果有这个键，就执行操作
     if delet_images_dict.get(parent_index_str) is not None:
+        # 如果列标中有这个子索引，就删掉这个子索引
         if int(son_index_str) in delet_images_dict[parent_index_str]:
             delet_images_dict[parent_index_str].remove(int(son_index_str))
+            # 如果删去后列表为空，则把相应的键一起删了
+            if not delet_images_dict[parent_index_str]:
+                delet_images_dict.pop(parent_index_str, None)
     return toml.dumps(delet_images_dict)
 
 
-# image_dir = r"C:\Users\WSH\Desktop\B站文件\冷门参数"
-# image_list = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.lower().endswith(('.jpg', '.png', '.jpeg', '.bmp'))]
+def auto_select() -> str:
+    
+    from imagededup.methods import PHash
+    import toml
+    import bisect
+    
+    global encodings, cluster_list
+    
+    if (encodings is None or not encodings) or (cluster_list is None or not cluster_list):
+        return "请先扫描"
+    
+    # 载入模型
+    phasher = PHash()
+    # 启发式算法获得待删除的列表，里面包含了图片名字字符串
+    auto_duplicates_list = phasher.find_duplicates_to_remove(encoding_map=encodings)
+    
+
+    # 定义一个空字典来存储映射关系
+    mapping = {}
+    # 遍历cluster_list中的每个集合
+    for i, s in enumerate(cluster_list):
+        # 遍历集合中的每个元素
+        for j, x in enumerate(s):
+            # 把元素作为键，把它所在的集合和位置作为值，存入字典
+            mapping[x] = (i, j)
+
+    # 定义输出的字典
+    result = { f"{i}":[] for i in range( len( cluster_list ) ) }
+    # 遍历lst1中的每个元素
+    for x in auto_duplicates_list:
+        # 获取它所在的集合和位置
+        i, j = mapping[x]
+        # 把第二个索引值加入到列表中
+        bisect.insort(result[f"{i}"], j)
+
+    return toml.dumps(result)
+
 
 def get_choose_image_index(evt: gr.SelectData):
     # evt.value 为标签 ；evt.index 为图片序号； evt.target 为调用组件名
@@ -114,6 +194,7 @@ with gr.Blocks(css="#delet_button {color: red}") as demo:
         with gr.Column(scale=10):
             images_dir = gr.Textbox(label="图片目录")
         with gr.Column(scale=1):
+            use_cache = gr.Checkbox(label="使用缓存")
             find_duplicates_images_button = gr.Button("扫描重复图片")
     with gr.Row():
         with gr.Column(scale=10):
@@ -124,11 +205,12 @@ with gr.Blocks(css="#delet_button {color: red}") as demo:
                 cancel_button = gr.Button("取消图片")
         with gr.Column(scale=1):
             delet_button = gr.Button("删除（不可逆）", elem_id="delet_button")
+            auto_select_button = gr.Button("自动选择")
             delet_images_str = gr.Textbox(label="待删除列表")
 
     # 按下后，在指定的目录搜索重复图像，并返回带标签的重复图像路径
     find_duplicates_images_button.click(fn=find_duplicates_images,
-                                        inputs=[images_dir],
+                                        inputs=[images_dir, use_cache],
                                         outputs=[duplicates_images_gallery, delet_images_str]
                                         )
 
@@ -144,11 +226,17 @@ with gr.Blocks(css="#delet_button {color: red}") as demo:
                          outputs=[delet_images_str]
                          )
 
-    # 按下后，将全局变量choose_image_index中的值加到列表中
+    # 按下后，将全局变量choose_image_index中的值从列表中删除
     cancel_button.click(fn=cancel,
                         inputs=[delet_images_str],
                         outputs=[delet_images_str]
                         )
+    
+    # 按下后，用启发式算法自动找出建议删除的重复项
+    auto_select_button.click(fn=auto_select,
+                             inputs=[],
+                             outputs=[delet_images_str]
+                            )
 
 
 demo.launch(debug=False, inbrowser=True)
